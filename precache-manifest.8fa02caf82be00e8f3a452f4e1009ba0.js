406f836d30709dcc7a3e08c84f07ad1eca209d5c
Brandon <bmwalshy@gmail.com>
2020-06-24 00:55:54 -0400

    Updates

---
+++ precache-manifest.8fa02caf82be00e8f3a452f4e1009ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d905ed313b93ddd9059c467c0cb05e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7393aadae0979619275",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ec0721197715dcf129b",
+    "url": "/static/js/2.41cc80d6.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.41cc80d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7393aadae0979619275",
+    "url": "/static/js/main.25adf2eb.chunk.js"
+  },
+  {
+    "revision": "59781d59281fa155f712",
+    "url": "/static/js/runtime-main.27849014.js"
+  }
+]);
